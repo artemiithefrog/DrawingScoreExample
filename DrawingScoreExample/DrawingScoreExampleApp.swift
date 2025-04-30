8a3a938f428af9b1a67a1bd3d5e9575a0c76c881
Artemii Malyshev <115695294+artemiithefrog@users.noreply.github.com>
2025-04-30 12:58:52 +0200

    Initial Commit

---
+++ DrawingScoreExample/DrawingScoreExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingScoreExampleApp.swift
+//  DrawingScoreExample
+//
+//  Created by artemiithefrog . on 30.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct DrawingScoreExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
